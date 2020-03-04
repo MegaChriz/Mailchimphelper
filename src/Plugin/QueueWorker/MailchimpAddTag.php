9a01de304cb640d8946db5c2a2dd81c2bb0970d4
MegaChriz <megachriz@hotmail.com>
2020-03-04 14:47:14 +0100

    Run queue 'mailchimphelper_tag_member' on cron runs, fixed instantiating this QueueWorker plugin.

--- src/Plugin/QueueWorker/MailchimpAddTag.php
+++ src/Plugin/QueueWorker/MailchimpAddTag.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mailchimphelper\Plugin\QueueWorker;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Queue\QueueWorkerBase;
 use Drupal\mailchimphelper\Helper;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -14,7 +15,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   title = @Translation("Mailchimp tag member"),
  * )
  */
-class MailchimpAddTag extends QueueWorkerBase {
+class MailchimpAddTag extends QueueWorkerBase implements ContainerFactoryPluginInterface {
 
   /**
    * Number of seconds in six hours.
@@ -33,18 +34,28 @@ class MailchimpAddTag extends QueueWorkerBase {
   /**
    * Constructs a new MailchimpAddTag object.
    *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
    * @param \Drupal\mailchimphelper\Helper $mailchimphelper
    *   The mailchimphelper service.
    */
-  public function __construct(Helper $mailchimphelper) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Helper $mailchimphelper) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->mailchimpHelper = $mailchimphelper;
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
       $container->get('mailchimphelper')
     );
   }
